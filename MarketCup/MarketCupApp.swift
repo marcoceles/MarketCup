232790a7457508b35ae3639e660529db94a44861
Marco Celestino <marco.celestino@ovolab.com>
2022-07-07 16:20:39 +0200

    Initial Commit

---
+++ MarketCup/MarketCupApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketCupApp.swift
+//  MarketCup
+//
+//  Created by Marco Celestino on 07/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MarketCupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
